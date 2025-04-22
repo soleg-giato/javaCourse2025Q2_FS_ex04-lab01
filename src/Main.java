9ece1ddce1de6a8fefe23e72244deb3295eef7c8
fedor shchukin <fedor.shchukin@gmail.com>
2025-04-22 19:07:20 +0300

    added categories

--- src/Main.java
+++ src/Main.java
@@ -16,26 +16,39 @@ public class Main {
     static int[][] arrBills = new int[1024][2];
     static int countCats = 0;
     static int countBills = 0;
-    static int amount = 0;
+    static int totalAmount = 0;
 
     public static void main(String[] args) {
 
         // тестовые данные
-        int idx0 = addCategory("Аванс");
-        System.out.printf("0 ?= %d", idx0);
+//        int idx0 = addCategory("Аванс");
+//        System.out.printf("0 ?= %d", idx0);
+//        System.out.println();
+//        int idx1 = addCategory("Продукты");
+//        System.out.printf("1 ?= %d", idx1);
+//        System.out.println();
+//        int idx2 = addCategory("Одежда");
+//        System.out.printf("2 ?= %d", idx2);
+//        System.out.println();
+//        int idx3 = addCategory("Аванс");
+//        System.out.printf("0 ?= %d", idx3);
+//        System.out.println();
+//        int idx4 = addCategory("Аванс");
+//        System.out.printf("0 ?= %d", idx4);
+//        System.out.println();
+
+
+        int idxBill0 = addBill("Аванс 56000");
+        System.out.printf("0 ?= %d", idxBill0);
         System.out.println();
-        int idx1 = addCategory("Продукты");
-        System.out.printf("1 ?= %d", idx1);
+        int idxBill1 = addBill("Продукты -37000");
+        System.out.printf("1 ?= %d", idxBill1);
         System.out.println();
-        int idx2 = addCategory("Одежда");
-        System.out.printf("2 ?= %d", idx2);
+        int idxBill2 = addBill("Аванс 128000");
+        System.out.printf("2 ?= %d", idxBill2);
         System.out.println();
-        int idx3 = addCategory("Аванс");
-        System.out.printf("0 ?= %d", idx3);
-        System.out.println();
-        int idx4 = addCategory("Аванс");
-        System.out.printf("0 ?= %d", idx4);
-        System.out.println();
+
+        printAllBills();
 
         printAllCats();
 
@@ -46,40 +59,85 @@ public class Main {
         System.out.println();
         System.out.println("Индексный массив категорий:");
         for (int i = 0; i < countCats; i++) {
-            System.out.printf("%d. %s", i+1, arrCategories[i]);
+            System.out.printf("%d. %s", i + 1, arrCategories[i]);
             System.out.println();
         }
         System.out.println();
     }
 
+    static void printAllBills() {
 
-    static int addCategory(String categoryName) {
+        System.out.println();
+        System.out.println("Ваши траты/пополнение:");
+        for (int i = 0; i < countBills; i++) {
+            System.out.printf("%d. %s %d", i + 1, getCategoryByIdx(arrBills[i][0]), arrBills[i][1]);
+            System.out.println();
+        }
+        System.out.printf("Всего операций %d, итоговая сумма счета: %d.", countBills, totalAmount);
+        System.out.println();
+
+    }
+
+
+    static int findCategory(String categoryName) {
 
         int recFound = -1;
 
         // пустой индексный массив категорий
         if (countCats == 0) {
-          arrCategories[0] = categoryName;
-          recFound = 0;
-          countCats++;
-        } else { // массив не пустой
+            return recFound;
+        } else {
+            // массив не пустой
             int i = 0;
 
             // ищем уже имеющуюся категорию
-            while (i < countCats || recFound > 0) {
+            while (i < countCats) {
                 if (categoryName.equalsIgnoreCase(arrCategories[i])) {
                     recFound = i;
                     return recFound;
                 }
                 i++;
             }
-            // если не нашли, добавляем новую
+        }
+
+        return recFound;
+    }
+
+    static int addCategory(String categoryName) {
+
+        int recFound = findCategory(categoryName);
+
+        // категория новая
+        if (recFound == -1) {
             arrCategories[countCats] = categoryName;
             recFound = countCats;
             countCats++;
         }
 
         return recFound;
+    }
+
+    static String getCategoryByIdx(int idx) {
+        return arrCategories[idx];
+    }
+
+    static int addBill(String bill) {
+        // validate();
+        String categoryName = bill.trim().split(" ")[0].trim();
+        int amount = Integer.parseInt(bill.trim().split(" ")[1]);
+
+        if (amount < 0 && totalAmount < Math.abs(amount)) {
+            System.out.printf("Сумма на счете \"%d\" меньше суммы списания \"%d\". Списание не прошло.", totalAmount, amount);
+            System.out.println();
+            return -1;
+        }
+
+        int catId = addCategory(categoryName);
+        arrBills[countBills] = new int[] {catId, amount};
+        countBills++;
+        totalAmount += amount;
+
+        return countBills;
     }
 
 }
